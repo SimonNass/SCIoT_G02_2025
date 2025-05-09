c6d6c249841dfd30472e0266c096711ca45cc76a
SimonNass <si.nass@t-online.de>
2025-05-09 14:26:41 +0200

    first led example

---
+++ room_unit_gateway/main.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+#
+# GrovePi Example for using the Grove LED for LED Fade effect (http://www.seeedstudio.com/wiki/Grove_-_LED_Socket_Kit)
+#
+# The GrovePi connects the Raspberry Pi and Grove sensors.  You can learn more about GrovePi here:  http://www.dexterindustries.com/GrovePi
+
+
+import time
+import grovepi
+
+# Connected sensors
+led1 = 5 #LED blue at D5 fade
+led2 = 4 #LED red at D4 on/off
+led3 = 2 #LED green at D2 on/off
+
+grovepi.pinMode(led1,"OUTPUT")
+grovepi.pinMode(led2,"OUTPUT")
+grovepi.pinMode(led3,"OUTPUT")
+
+
+time.sleep(1)
+i = 0
+togle = 0
+while True:
+    try:
+        # Reset
+        if i > 255:
+            i = 0
+
+        if togle == 1:
+            togle = 0
+        elif togle == 0:
+            togle = 1
+
+        # Current brightness
+        print ("LED dim:{}".format(i))
+        print ("LED togle {}".format(togle))
+
+        # Give PWM output to LED
+        grovepi.analogWrite(led1,i)
+        grovepi.digitalWrite(led2,togle)
+        grovepi.digitalWrite(led3,togle)
+
+        # Increment brightness for next iteration
+        i = i + 20
+        time.sleep(.5)
+
+    except KeyboardInterrupt:
+        grovepi.analogWrite(led1,0)
+        grovepi.digitalWrite(led2,0)
+        grovepi.digitalWrite(led3,0)
+        break
+    except IOError:
+        print ("Error")
