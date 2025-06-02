411649d59f49fb072b6d47e93ab4c814a3d18eb8
SimonNass <si.nass@t-online.de>
2025-06-02 21:04:37 +0200

    generalized write_actuator for int + bug self.last_value != write_value

--- room_unit_gateway/actuators/actuator.py
+++ room_unit_gateway/actuators/actuator.py
@@ -39,8 +39,21 @@ class ActuatorInterface(ABC):
     def __dict__(self):
         return {"id":str(self.id),"name":self.name,"type_name":self.type,"connector":self.i2c_connector,"connector_type":str(self.connector_type),"min":self.min_value, "max":self.max_value, "datatype":self.datatype, "unit":self.unit, "initial_value":self.initial_value, "off_value":self.off_value, "last_value":self.last_value}
 
-    @abstractmethod
     def write_actuator(self, value: int):
+        write_value = max(self.min_value,min(self.max_value,value))
+        if not self.is_valid(write_value):
+            text = "value {} is out of the allowed interval [{},{}] for this actuator".format(value,self.min_value,self.max_value)
+            raise ValueError(text)
+        try:
+            _ = self.write_internal_actuator(write_value)
+            self.last_value = write_value
+            print ("{}: {}".format(self.name,self.last_value))
+        except Exception as e:
+            print ("write was unsucesful")
+            print (e)
+
+    @abstractmethod
+    def write_internal_actuator(self, value: int):
         pass
 
     def is_valid(self, value: int):
@@ -60,18 +73,8 @@ class AnalogActuator(ActuatorInterface):
     def __del__(self):
         grovepi.analogWrite(self.i2c_connector,self.off_value)
 
-    def write_actuator(self, value: int):
-        write_value = max(self.min_value,min(self.max_value,value))
-        if not self.is_valid(write_value):
-            text = "value {} is out of the allowed interval [{},{}] for this actuator".format(value,self.min_value,self.max_value)
-            raise ValueError(text)
-        try:
-            self.last_value = grovepi.analogWrite(self.i2c_connector,write_value)
-            #self.last_value = write_value
-            print ("{}: {}".format(self.name,self.last_value))
-        except Exception as e:
-            print ("write was unsucesful")
-            print (e)
+    def write_internal_actuator(self, write_value: int):
+        return grovepi.analogWrite(self.i2c_connector,write_value)
 
 class DigitalActuator(ActuatorInterface):
     def __init__(self, name: str, type_name: str, connector: int, connector_types: Connectortype, min_value: int, max_value: int, datatype: str, unit: str, initial_value: int, off_value: int):
@@ -83,15 +86,5 @@ class DigitalActuator(ActuatorInterface):
     def __del__(self):
         grovepi.digitalWrite(self.i2c_connector,self.off_value)
 
-    def write_actuator(self, value: int):
-        write_value = max(self.min_value,min(self.max_value,value))
-        if not self.is_valid(write_value):
-            text = "value {} is out of the allowed interval [{},{}] for this actuator".format(value,self.min_value,self.max_value)
-            raise ValueError(text)
-        try:
-            self.last_value = grovepi.digitalWrite(self.i2c_connector,write_value)
-            #self.last_value = write_value
-            print ("{}: {}".format(self.name,self.last_value))
-        except Exception as e:
-            print ("write was unsucesful")
-            print (e)
+    def write_internal_actuator(self, write_value: int):
+        return grovepi.digitalWrite(self.i2c_connector,write_value)
