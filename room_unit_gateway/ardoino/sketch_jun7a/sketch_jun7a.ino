5f0f32db07fc7dfd4567c3068387f61ca172107b
SimonNass <si.nass@t-online.de>
2025-06-09 13:33:57 +0200

    handleRequest methode

--- room_unit_gateway/ardoino/sketch_jun7a/sketch_jun7a.ino
+++ room_unit_gateway/ardoino/sketch_jun7a/sketch_jun7a.ino
@@ -49,22 +49,15 @@ void setup() {
 }
 
 void loop() {
-  delay(2000);
-  Serial.println("---");
-  senseSound();
-  senseHumidity();
-  senseTemperature();
+  //delay(2000);
+  //Serial.println("---");
+  //senseSound();
+  //senseHumidity();
+  //senseTemperature();
   //activateRFID();
+  //activateServo(0);
   if (string_complete) {
-    String type = input_str.substring(0,0+1);
-    String body = input_str.substring(0+2,input_str.length());
-    if (type.equals("s")) {
-      activateServo(body.toInt());
-    }
-    delay(15);
-    //exit(0);
-    input_str = "";
-    string_complete = false;
+    handleRequest(input_str);
   }
 }
 
@@ -79,24 +72,46 @@ void serialEvent() {
   }
 }
 
+void handleRequest(String input) {
+    String type = input.substring(0,0+1);
+    String body = input.substring(0+2,input.length());
+    if (type.equals("motor")) {
+      activateServo(body.toInt());
+    } else if (type.equals("soundlevel")) {
+      senseSound();
+    } else if (type.equals("humidity")) {
+      senseHumidity();
+    } else if (type.equals("temperature")) {
+      senseTemperature();
+    } else if (type.equals("rfid")) {
+      //activateRFID();
+    } else if (type.equals("exit")) {
+      exit(0);
+    }
+    //delay(15);
+    input_str = "";
+    string_complete = false;
+}
+
 void activateRFID() {
-  Serial.println("start search");
-  delay(1000);
+  //Serial.println("start search");
   if (!mfrc522.PICC_IsNewCardPresent())
   {
-     Serial.println("stop");
+    delay(1000);
+    //Serial.println("stop");
     // Wenn keine Karte in Reichweite ist ..
     // .. wird die Abfrage wiederholt.
     return;
   }
-  Serial.println("new");
+  //Serial.println("new");
   if (!mfrc522.PICC_ReadCardSerial())
   {
+    delay(1000);
     // Wenn kein RFID-Sender ausgewählt wurde ..
     // .. wird die Abfrage wiederholt.
     return;
   }
-  Serial.println("Karte entdeckt!");
+  //Serial.println("Karte entdeckt!");
   String WertDEZ;
   // Dezimal-Wert in Strings schreiben
   for (byte i = 0; i < mfrc522.uid.size; i++)
@@ -105,7 +120,8 @@ void activateRFID() {
     WertDEZ = WertDEZ + String(mfrc522.uid.uidByte[i], DEC) + " ";
   }
   // Kennung dezimal anzeigen
-  Serial.println("Dezimalwert: " + WertDEZ);
+  Serial.print("RFIDuid: ");
+  Serial.println(WertDEZ);
   // kurze Pause, damit nur ein Wert gelesen wird
   delay(1000);
 }
@@ -114,6 +130,7 @@ void activateServo(int pos) {
   int val = max(min(180,pos),0);
   if (val >= 0 && val <= 180) {
     servo.write(val);
+    Serial.print("Servomotor: ");
     Serial.println(val);
   }
 }
