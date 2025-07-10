c9230b0789f77cc6ee5d9d3f4a81419afcf141fe
SimonNass <si.nass@t-online.de>
2025-07-10 22:04:13 +0200

    additional try logging satatements

--- room_unit_gateway/config_reader.py
+++ room_unit_gateway/config_reader.py
@@ -7,83 +7,114 @@ logger = logging.getLogger(__name__)
 import object_factory
 
 def read_general_config(config):
-    #print ("reading in general", flush=True)
-    logger.info("reading in general")
-    # General
-    version = config.get('General', 'version', fallback=0)
-    if version == 2:
-        print ("Error wrong config version")
-        logger.warning("Error wrong config version")
-    max_cycle_time = config.get('General', 'max_cycle_time', fallback=100)
-    return max_cycle_time
+    try:
+        #print ("reading in general", flush=True)
+        logger.info("reading in general")
+        # General
+        version = config.get('General', 'version', fallback=0)
+        if version == 2:
+            print ("Error wrong config version")
+            logger.warning("Error wrong config version")
+        max_cycle_time = config.get('General', 'max_cycle_time', fallback=100)
+        return max_cycle_time
+    except Exception as e:
+        logger.error(f"Reading config file was not succesfully in the General section {e}")
+        raise e
 
 def read_mqtt_config(config):
-    #print ("reading in MQTT", flush=True)
-    logger.info("reading in MQTT")
-    mqtt_name = config.get('MQTT', 'name', fallback='MQTT')
-    mqtt_host = config.get('MQTT', 'host', fallback='0.0.0.0')
-    mqtt_port = config.getint('MQTT', 'port', fallback=1884)
-    mqtt_username = config.get('MQTT', 'username', fallback='root')
-    return mqtt_name, mqtt_host, mqtt_port, mqtt_username
+    try:
+        #print ("reading in MQTT", flush=True)
+        logger.info("reading in MQTT")
+        mqtt_name = config.get('MQTT', 'name', fallback='MQTT')
+        mqtt_host = config.get('MQTT', 'host', fallback='0.0.0.0')
+        mqtt_port = config.getint('MQTT', 'port', fallback=1884)
+        mqtt_username = config.get('MQTT', 'username', fallback='root')
+        return mqtt_name, mqtt_host, mqtt_port, mqtt_username
+    except Exception as e:
+        logger.error(f"Reading config file was not succesfully in the MQTT section {e}")
+        raise e
 
 def read_architecture_config(config):
-    #print ("reading in Architecture", flush=True)
-    logger.info("reading in Architecture")
-    floor_id = config.get('Architecture', 'floor_ID', fallback=0)
-    max_rooms_per_floor = config.get('MQArchitectureTT', 'max_rooms_per_floor', fallback=100)
-    room_id = config.get('Architecture', 'room_ID', fallback=0)
-    room_info = object_factory.configure_room_inof(floor_id=floor_id,
-                                                   max_rooms_per_floor=max_rooms_per_floor,
-                                                   room_id=room_id)
-    return room_info
+    try:
+        #print ("reading in Architecture", flush=True)
+        logger.info("reading in Architecture")
+        floor_id = config.get('Architecture', 'floor_ID', fallback=0)
+        max_rooms_per_floor = config.get('MQArchitectureTT', 'max_rooms_per_floor', fallback=100)
+        room_id = config.get('Architecture', 'room_ID', fallback=0)
+        room_info = object_factory.configure_room_info(floor_id=floor_id,
+                                                    max_rooms_per_floor=max_rooms_per_floor,
+                                                    room_id=room_id)
+        return room_info
+    except Exception as e:
+        logger.error(f"Reading config file was not succesfully in the Architecture section {e}")
+        raise e
 
 def read_ardoino_config(config):
-    #print ("reading in ardoino", flush=True)
-    logger.info("reading in ardoino")
-    # Ardoino connection
-    message_end_signal = config.get('Ardoino', 'message_end_signal', fallback='')
-    usb_channel_type = config.get('Ardoino', 'usb_channel_type_default', fallback='/dev/ttyACM0')
-    usb_channel_data_rate = config.get('Ardoino', 'usb_channel_data_rate', fallback=9600)
-    ardoino_serial = object_factory.configure_ardoino_connection(message_end_signal=message_end_signal,
-                                                                 usb_channel_type=usb_channel_type,
-                                                                 usb_channel_data_rate=usb_channel_data_rate)
-    return ardoino_serial
+    try:
+        #print ("reading in ardoino", flush=True)
+        logger.info("reading in ardoino")
+        # Ardoino connection
+        message_end_signal = config.get('Ardoino', 'message_end_signal', fallback='')
+        usb_channel_type = config.get('Ardoino', 'usb_channel_type_default', fallback='/dev/ttyACM0')
+        usb_channel_data_rate = config.get('Ardoino', 'usb_channel_data_rate', fallback=9600)
+        ardoino_serial = object_factory.configure_ardoino_connection(message_end_signal=message_end_signal,
+                                                                    usb_channel_type=usb_channel_type,
+                                                                    usb_channel_data_rate=usb_channel_data_rate)
+        return ardoino_serial
+    except Exception as e:
+        logger.error(f"Reading config file was not succesfully in the Ardoino section {e}")
+        raise e
 
 def read_sensors_config(config, ardoino_serial):
-    #print ("reading in sensors", flush=True)
-    logger.info("reading in sensors")
-    # Sensors
-    sensor_types = object_factory.configure_sensor_types(config.get('Sensors','sensor_types', fallback="[]"))
-    sensor_class_list = object_factory.configure_sensors(config.get('Sensors','sensor_list', fallback="[]"), 
-                                                         sensor_types, 
-                                                         ardoino_serial)
-    return sensor_class_list
+    try:
+        #print ("reading in sensors", flush=True)
+        logger.info("reading in sensors")
+        # Sensors
+        sensor_types = object_factory.configure_sensor_types(config.get('Sensors','sensor_types', fallback="[]"))
+        sensor_class_list = object_factory.configure_sensors(config.get('Sensors','sensor_list', fallback="[]"), 
+                                                            sensor_types, 
+                                                            ardoino_serial)
+        return sensor_class_list
+    except Exception as e:
+        logger.error(f"Reading config file was not succesfully in the Sensors section {e}")
+        raise e
 
 def read_actuators_config(config, ardoino_serial):
-    #print ("reading in actuators", flush=True)
-    logger.info("reading in actuators")
-    # Actuators
-    actuator_types = object_factory.configure_actuator_types(config.get('Actuators','actuator_types', fallback="[]"))
-    actuator_class_list = object_factory.configure_actuators(config.get('Actuators','actuator_list', fallback="[]"), 
-                                                             actuator_types, 
-                                                             ardoino_serial)
-    return actuator_class_list
+    try:
+        #print ("reading in actuators", flush=True)
+        logger.info("reading in actuators")
+        # Actuators
+        actuator_types = object_factory.configure_actuator_types(config.get('Actuators','actuator_types', fallback="[]"))
+        actuator_class_list = object_factory.configure_actuators(config.get('Actuators','actuator_list', fallback="[]"), 
+                                                                actuator_types, 
+                                                                ardoino_serial)
+        return actuator_class_list
+    except Exception as e:
+        logger.error(f"Reading config file was not succesfully in the Actuators section {e}")
+        raise e
 
 def read_virtual_environment_config(config, sensor_class_list, actuator_class_list):
-    #print ("reading in VirtualEnfironment mapping", flush=True)
-    logger.info("reading in VirtualEnfironment mapping")
-    # VirtualEnfironment
-    virtual_enfironment_list = object_factory.configure_environment_map(config.get('VirtualEnfironment','virtual_enfironment_list', fallback="[]"))
-    virtual_environment = object_factory.configure_environment(sensors=sensor_class_list,
-                                                               actuators=actuator_class_list,
-                                                               virtual_enfironment_list=virtual_enfironment_list)
-    return virtual_environment
+    try:
+        #print ("reading in VirtualEnfironment mapping", flush=True)
+        logger.info("reading in VirtualEnfironment mapping")
+        # VirtualEnfironment
+        virtual_enfironment_list = object_factory.configure_environment_map(config.get('VirtualEnfironment','virtual_enfironment_list', fallback="[]"))
+        virtual_environment = object_factory.configure_environment(sensors=sensor_class_list,
+                                                                actuators=actuator_class_list,
+                                                                virtual_enfironment_list=virtual_enfironment_list)
+        return virtual_environment
+    except Exception as e:
+        logger.error(f"Reading config file was not succesfully in the VirtualEnfironment section {e}")
+        raise e
 
 def read_config(config_file_name, password: str):
     print (f"reading in {config_file_name}", flush=True)
-    config = configparser.ConfigParser(interpolation=None)
-    config.read(config_file_name, encoding='utf-8')
-    #config.read('config.ini')
+    try:
+        config = configparser.ConfigParser(interpolation=None)
+        config.read(config_file_name, encoding='utf-8')
+    except Exception as e:
+        logger.error(f"Reading config file {config_file_name} was not succesfull {e}")
+        raise e
 
     max_cycle_time = read_general_config(config)
 
