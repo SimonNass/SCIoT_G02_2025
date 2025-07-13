6a617d2e8c03af32cc12d97fabc77eadb72d7c2c
SimonNass <si.nass@t-online.de>
2025-07-13 03:30:18 +0200

    configuration file

--- aiplaning/pddl_converter_input.py
+++ aiplaning/pddl_converter_input.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 """Module specifies the data input to generate the pddl domain and problem based on the data in the backend DB."""
 
+import json
+import os
+import configparser
+
 def query_input_over_db():
     domain_name = "test_SCIoT_G02_2025"
     problem_name = 'test'
@@ -102,78 +106,51 @@ def query_input_over_db():
             }
 
 def query_input_over_config_file():
-    domain_name = "test_SCIoT_G02_2025"
-    problem_name = 'test'
 
-    floor_uids = ['f0','f1']
-    room_uids_per_floor = {'f0':['r0','r1','r3'],'f1':['r2']}
+    config_file_name: os.path = "aiplaning/config/ai_planer_test_example.ini"
+    print (f"reading in {config_file_name}", flush=True)
+    try:
+        config = configparser.ConfigParser(interpolation=None)
+        config.read(config_file_name, encoding='utf-8')
+    except Exception as e:
+        print(e)
+
+    # General
+    _ = config.get('General', 'version', fallback=0)
+    domain_name = config.get('General', 'domain_name', fallback="test_SCIoT_G02_2025")
+    problem_name = config.get('General', 'problem_name', fallback="test")
+    plan_cleaning = config.get('General', 'plan_cleaning', fallback=False)
+
+    # Topology
+    floor_uids = json.loads(config.get('Topology', 'floor_uids', fallback='[]'))
+    room_uids_per_floor = json.loads(config.get('Topology', 'room_uids_per_floor', fallback='{}'))
     # TODO what if sensor with room is not part of anny floor?
-    room_occupied_initial_values = {'r0':False, 'r1': True, 'r2':False, 'r3': True}
-    #floor_list = db.list_all_floors()
-    #floor_uids = [floor['id'] for floor in floor_list]
-    #room_uids_per_floor = {floor['id']:floor['rooms'] for floor in floor_list}
-    #for floor in floor_list:
-    #    room_occupied_initial_values = {room['id']:room['is_occupied'] for room in room_uids_per_floor[floor]}
+    room_occupied_initial_values = json.loads(config.get('Topology', 'room_occupied_initial_values', fallback='{}'))
+    elevator_uids = json.loads(config.get('Topology', 'elevator_uids', fallback='[]'))
+    names_room_positions = json.loads(config.get('Topology', 'names_room_positions', fallback='[]'))
+    sensor_room_mapping = json.loads(config.get('Topology', 'sensor_room_mapping', fallback='{}'))
+    actuator_room_mapping = json.loads(config.get('Topology', 'actuator_room_mapping', fallback='{}'))
+    cleaning_team_uids = json.loads(config.get('Topology', 'cleaning_team_uids', fallback='[]'))
 
-    elevator_uids = ['e0','e1']
-    cleaning_team_uids = ['cleaning_team_1','cleaning_team_2']
-    names_room_positions = ['overall_room']
-    #['overall_room', 'bed', 'closet', 'window']
+    # IoT
+    sensor_types = json.loads(config.get('IoT', 'sensor_types', fallback='{}'))
+    actuator_increases_sensor_mapping_matrix = json.loads(config.get('IoT', 'actuator_increases_sensor_mapping_matrix', fallback='{}'))
+    actuator_decreases_sensor_mapping_matrix = json.loads(config.get('IoT', 'actuator_decreases_sensor_mapping_matrix', fallback='{}'))
+    sensor_initial_values = json.loads(config.get('IoT', 'sensor_initial_values', fallback='{}'))
+    sensor_goal_values = json.loads(config.get('IoT', 'sensor_goal_values', fallback='{}'))
+    actuator_initial_values = json.loads(config.get('IoT', 'actuator_initial_values', fallback='{}'))
+    sensor_initial_locked = json.loads(config.get('IoT', 'sensor_initial_locked', fallback='[]'))
+    sensor_goal_state_mapping = json.loads(config.get('IoT', 'sensor_goal_state_mapping', fallback='{}'))
 
-    sensor_room_mapping = {'r0':['s1'], 'r2':['s2'], 'r3':['s3', 's4']}
-    actuator_room_mapping = {'r0':['a1'], 'r2':['a2']}
-    #for floor in floor_uids:
-    #    for room in room_uids_per_floor[floor]:
-    #        device_list = db.list_devices_in_room(floor,room)['devices']
-    #        sensor_uids = []
-    #        actuator_uids = []
-    #        for device in device_list:
-    #            if device is_sensor():
-    #                sensor_uids.append(device)
-    #            elif device is_actuator():
-    #                actuator_uids.append(device)
-    #        sensor_room_mapping.update({room:sensor_uids})
-    #        actuator_room_mapping.update({room:actuator_uids})
-
-    sensor_types = {'s1':'light_s', 's2':'humidity_s', 's3':'temperature_s', 's4':'temperature_s'}
-
-    # TODO get info from db
-    actuator_increases_sensor_mapping_matrix = {'a1':['s1','s2'], 'a2':['s2']}
-    actuator_decreases_sensor_mapping_matrix = {'a1':['s1']}
-
-    sensor_initial_values = {'s1': -1, 's2':1, 's3':-1}
-    sensor_goal_values = {'s1': -1, 's2':1}
-    actuator_initial_values = {'a1': True, 'a2':False}
-    #for floor in floor_uids:
-    #    for room in room_uids_per_floor[floor]:
-    #        for device in sensor_room_mapping[room]:
-    #            curent_value = db.request_current_value_hierarchical(floor,room,device)
-    #            sensor_initial_values.update({device:curent_value})
-    #        for device in actuator_room_mapping[room]:
-    #            curent_value = db.request_current_value_hierarchical(floor,room,device)
-    #            sensor_initial_values.update({device:curent_value})
-
-    sensor_initial_locked = ['s4']
-
-    sensor_goal_state_mapping = {'temperature_s':'is_ok',
-                                 'humidity_s':'is_ok',
-                                 'window_rotation_s':'is_low',
-                                 'light_s':'is_high',
-                                 'sound_s':'is_ok'
-                                 }
-
+    # Activity
     # TODO make 'requests_service':{'button_s':'is_sensing' -> request text displayed
     # TODO fine tune sensor ideal position for the activitys
-    activity_mapping = {'bath':{'temperature_s':'is_ok'},
-                        'read':{'temperature_s':'is_ok', 'light_s':'is_high', 'sound_s':'is_ok'},
-                        'sleep':{'light_s':'is_low', 'sound_s':'is_low'},
-                        }
+    activity_mapping = json.loads(config.get('Activity', 'activity_mapping', fallback='{}'))
 
-
-    return {'domain_name':domain_name,
+    input_dictionary =  {'domain_name':domain_name,
             'problem_name':problem_name,
 
-            'plan_cleaning':False,
+            'plan_cleaning':plan_cleaning,
 
             'floor_uids':floor_uids,
             'room_uids_per_floor':room_uids_per_floor,
@@ -200,6 +177,8 @@ def query_input_over_config_file():
             'activity_mapping':activity_mapping,
             'sensor_goal_state_mapping':sensor_goal_state_mapping,
             }
+    print (input_dictionary)
+    return input_dictionary
 
 def query_input(over_config_file: bool = False):
     if over_config_file:
@@ -227,6 +206,6 @@ def query_input(over_config_file: bool = False):
 #   'sensor_goal_values':sensor_goal_values, > Dict[uuid as str, simplified value]
 #   'actuator_initial_values':actuator_initial_values, > Dict[uuid as str, bool]
 #   'room_occupied_initial_values':room_occupied_initial_values, > Dict[uuid as str, bool]
-#   'activity_mapping':activity_mapping,
-#   'sensor_goal_state_mapping':sensor_goal_state_mapping,
+#   'activity_mapping':activity_mapping, > Dict[str, Dict[str,str]]
+#   'sensor_goal_state_mapping':sensor_goal_state_mapping, > Dict[str, str]
 #   }
